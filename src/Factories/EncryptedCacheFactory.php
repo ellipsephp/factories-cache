7af037c7c4503b6ced0d8a2fad1d87f5e5db519f
pmall <pierre.mallinjoud@gmail.com>
2017-02-22 11:55:38 +0100

    add encrypted cache factory

---
+++ src/Factories/EncryptedCacheFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Pmall\Cache\Factories;
+
+use Psr\Cache\CacheItemPoolInterface;
+
+use Defuse\Crypto\Key;
+
+use Cache\Encryption\EncryptedCachePool;
+use Cache\Taggable\TaggablePSR6PoolAdapter;
+
+class EncryptedCacheFactory
+{
+    /**
+     * Make a new encrypted cache using the given cache and the given encryption
+     * key.
+     *
+     * @param Psr\Cache\CacheItemPoolInterface  $cache
+     * @param string                            $key_str
+     * @return Psr\Cache\CacheItemPoolInterface
+     */
+    public function __invoke(CacheItemPoolInterface $cache, string $key_str): CacheItemPoolInterface
+    {
+        $taggable = TaggablePSR6PoolAdapter::makeTaggable($cache);
+
+        $key = Key::loadFromAsciiSafeString($key_str);
+
+        return new EncryptedCachePool($taggable, $key);
+    }
+}
